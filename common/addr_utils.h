42442ba6284a47e319322478067c903db6eb11f9
Adrian Figueroa <adrian.figueroa@metirionic.com>
2026-07-08 10:00:21 +0200

    chore: clean up

--- common/addr_utils.h
+++ common/addr_utils.h
@@ -8,12 +8,12 @@
  *  @brief Shared BLE address utility functions
  */
 
-#ifndef ADDR_UTILS_H__
-#define ADDR_UTILS_H__
+#ifndef ADDR_UTILS_H
+#define ADDR_UTILS_H
 
 #include <stdint.h>
 #include <zephyr/bluetooth/conn.h>
 
 uint64_t addr_to_u64(const bt_addr_le_t * p_addr);
 
-#endif /* ADDR_UTILS_H__ */
+#endif /* ADDR_UTILS_H */
